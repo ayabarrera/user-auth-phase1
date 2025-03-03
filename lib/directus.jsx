2ccc19676e7a726d36264208a75c187643f6e8e1
ayabarrera <alyannabarrera@gmail.com>
2025-03-03 15:34:30 -0700

    added directus js under root -> lib folder

---
+++ lib/directus.jsx
@@ -0,0 +1,7 @@
+import { authentication, createDirectus, rest } from "@directus/sdk";
+
+const client = createDirectus(process.env.DIRECTUS_URL)
+  .with(rest())
+  .with(authentication("cookie", { credentials: "include" }));
+
+export default client;
