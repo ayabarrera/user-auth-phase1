9b35054827064f16097f828020221f77e603b3bb
ayabarrera <alyannabarrera@gmail.com>
2025-03-07 00:20:32 -0700

    route.jsx for register

--- app/api/auth/register/route.jsx
+++ app/api/auth/register/route.jsx
@@ -7,17 +7,23 @@ export async function POST(request) {
 
     const email = formData.get("email");
     const password = formData.get("password");
+    const firstName = formData.get("first_name");
+    const lastName = formData.get("last_name");
 
-    if (!email || !password) {
+    if (!email || !password || !firstName || !lastName) {
         return NextResponse.json({ error: "All fields are required" }, { status: 400 });
     }
 
     try {
-        await client.request(registerUser(email, password));
+        await client.request(registerUser(email, password, {
+            first_name: firstName,
+            last_name: lastName,
+        }));
         const url = request.nextUrl.clone();
         url.pathname = "/dashboard"
         return NextResponse.redirect(url);
-    } catch {
+    } catch (error) {
+        console.error("Directus Registration Error: ", error);
         return NextResponse.json({ error: "Registration failed" }, { status: 500 });
     }
 }
